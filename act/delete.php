5f01286c0df5afafd16d75a47325060f76ab8538
Imanuel Harkespan <octone.harry@gmail.com>
2016-12-05 23:46:35 -0800

    delete data beserta filenya

--- act/delete.php
+++ act/delete.php
@@ -4,6 +4,10 @@
 	$del = new Kendaraan();
 
 	$id = $_GET['id'];
+	$data = $del->getKendaraan($id);
+	$d = mysqli_fetch_array($data);
+	
+	unlink("../uploads/".$d['files']);
 	$del->delKendaraan($id);
 
 	header("Location:read.php");
