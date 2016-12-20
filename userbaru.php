821e6f9a40471ce2f29dc90acd76e5a48e9b3b20
Imanuel Harkespan <octone.harry@gmail.com>
2016-12-20 14:38:11 +0700

    membuat form pendaftaran user baru

---
+++ userbaru.php
@@ -0,0 +1,6 @@
+<form method="post" action="act/saveuser.php">
+Username : <input type="text" name="uname"><br/>
+Password : <input type="password" name="upass"><br/>
+Nama Lengkap : <input type="text" name="nama"><br/>
+<input type="submit" value="Simpan">
+</form>
