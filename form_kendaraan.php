89e4598dd5ea0bb66b8666691ee7316ddfa0b2d3
Imanuel Harkespan <octone.harry@gmail.com>
2016-12-20 14:34:33 +0700

    menambah session untuk ngecek sudah login apa belum

--- form_kendaraan.php
+++ form_kendaraan.php
@@ -1,3 +1,15 @@
+<?php
+	session_start();
+	if($_SESSION['is_logged']!=true)
+	{
+		header("Location:login.php?logout=true");
+		exit;
+	}
+?>
+<h4>Selamat Datang, <strong><?php echo $_SESSION['nama']?></strong></h4>
+<p>
+informasi : Anda login pada <?php echo date("d m Y H:i:s"),", browser : ", $_SERVER['HTTP_USER_AGENT'],", IP: ", $_SERVER['REMOTE_ADDR']?>
+</p>
 <form method="post" enctype="multipart/form-data" action="act/simpan.php">
 Nama Kendaraan : <input type="text" name="nama"><br/>
 Tahun Pembuatan : <input type="text" name="tahun"><br/>
@@ -8,4 +20,5 @@ Gambar : <input type="file" name="userfile"><br/>
 
 <br/>
 <br/>
-<a href="act/read.php">Lihat hasil input data</a>
+<a href="act/read.php">Lihat hasil input data</a><br/>
+<a href="login.php?logout=true">Logout</a><br/>
